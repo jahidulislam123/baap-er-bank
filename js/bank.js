8e84d12d61dc5f2dfa4499518ea2c49b6f0033d4
unknown <islam15-3879@diu.edu.bd>
2022-02-10 08:38:48 +0600

    secondCommited

---
+++ js/bank.js
@@ -0,0 +1,18 @@
+document.getElementById('login-submit').addEventListener('click',function(){
+    // get user email 
+    const emailField =document.getElementById('user-email');
+    const userEmail =emailField.value;
+    //console.log(userEmail);
+    // get useer password
+    const passwordField =document.getElementById('user-password');
+    const userPassword =passwordField.value;
+    //console.log(userPassword);
+
+    if(userEmail=='sontan@gmail.com'&& userPassword=='secret'){
+      //console.log('valid user');  
+      window.location.href='banking.html';
+    }else{
+        console.log('invalid');
+    }
+    
+})
