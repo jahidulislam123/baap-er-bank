a12bafbcaa4ebeb989ac9b1df9d953b0a9786420
unknown <islam15-3879@diu.edu.bd>
2022-02-10 12:44:41 +0600

    added

--- js/banking.js
+++ js/banking.js
@@ -26,4 +26,59 @@ document.getElementById('deposit-button').addEventListener('click',function(){
     balanceTotal.innerText=newBalanceTotal;
     depositInput.value='';
 
-})
+});
+
+// handle withdraw evenet handler
+document.getElementById('withdraw-button').addEventListener('click',function(){
+
+    const withdrawInput =document.getElementById('withdraw-input');
+    const withdrawAmountText =withdrawInput.value;
+    const newWithdrawAmount=parseFloat(withdrawAmountText);
+    console.log(newWithdrawAmount);
+    // set withdraw total
+    const withdrawTotal =document.getElementById('withdraw-total');
+    const previousWithdrawTotalText =withdrawTotal.innerText;
+    const previousbalanceTotal=parseFloat(previousWithdrawTotalText);
+
+    const newWithdrawTotal =previousbalanceTotal+newWithdrawAmount;
+    withdrawTotal.innerText=newWithdrawTotal;
+    
+    // update balance after withdraw 
+    const balanceTotalww=document.getElementById('balance-total');
+    const previousbalancetextw =balanceTotalww.innerText;
+    // console.log('clicked');
+    const previousbalanceTotalww=parseFloat(previousbalancetextw);
+    const newBalanceTotalww=previousbalanceTotalww-newWithdrawAmount;
+    balanceTotalww.innerText=newBalanceTotalww;
+    withdrawInput.value='';
+    
+
+
+});
+
+
+// document.getElementById('withdraw-button').addEventListener('click',function(){
+
+//     const withdrawInput =document.getElementById('withdraw-input');
+//     const withdrawAmountText =withdrawInput.value;
+//     const newWithdrawAmount=parseFloat(withdrawAmountText);
+//     console.log(newWithdrawAmount);
+//     // set withdraw total
+//     // const withdrawTotal =document.getElementById('withdraw-total');
+//     // const previousWithdrawTotalText =withdrawTotal.innerText;
+//     // const previousbalanceTotal=parseFloat(previousWithdrawTotalText);
+
+//     // const newWithdrawTotal =previousbalanceTotal+newWithdrawAmount;
+//     // withdrawTotal.innerText=newWithdrawTotal;
+    
+//     // update balance after withdraw 
+//     const balanceTotal=document.getElementById('balance-total');
+//     const previousbalancetext =balanceTotal.innerText;
+//     // console.log('clicked');
+//     const previousbalanceTotal=parseFloat(previousbalancetext);
+//     const newBalanceTotal=previousbalanceTotal-newWithdrawAmount;
+//     balanceTotal.innerText=newBalanceTotal;
+//     withdrawInput.value='';
+
+
+// })
